f50ff343e14a4b767340bcaf9c1a1d404fe932a7
Luis Hernandez <ldhdv95@gmail.com>
2021-05-21 07:06:52 -0500

    Add history service

--- src/controllers/historyCtrl.js
+++ src/controllers/historyCtrl.js
@@ -1,53 +1,13 @@
 /* eslint-disable camelcase */
-const { v4: uuidv4 } = require('uuid');
-const validator = require('validator');
-const omit = require('lodash/omit');
-const paginate = require('../utils/paginate');
-const db = require('../models');
-
-const Patient = db.patient;
-const History = db.history;
-const City = db.city;
-const State = db.state;
-const Code = db.code;
-const Agreement = db.agreement;
-const { Op } = db.Sequelize;
+const {
+  getHistory, getAllHistories, updateHistory, createHistory,
+} = require('../services/historyService');
 
 exports.id = async (req, res, next, id) => {
   try {
-    if (validator.isUUID(id)) {
-      const history = await History.findOne({
-        where: {
-          id,
-        },
-        include: [
-          {
-            model: Code,
-          },
-          {
-            model: Patient,
-            include: {
-              model: City,
-              include: [State],
-            },
-          },
-        ],
-      });
-      if (history) {
-        req.history = history;
-        next();
-      } else {
-        next({
-          statusCode: '404',
-          message: 'Resource not found',
-        });
-      }
-    } else {
-      next({
-        statusCode: '404',
-        message: 'Resource not found',
-      });
-    }
+    const history = await getHistory(id);
+    req.history = history;
+    next();
   } catch (error) {
     next(error);
   }
@@ -56,29 +16,12 @@ exports.id = async (req, res, next, id) => {
 exports.all = async (req, res, next) => {
   const { page = 0, pageSize = 10 } = req.query;
   try {
-    const { rows, count } = await History.findAndCountAll({
-      where: {},
-      ...paginate({ page, pageSize }),
-      include: [
-        {
-          model: Patient,
-          include: [{
-            model: City,
-            include: [State],
-          }],
-        },
-      ],
-    });
-    const pages = Math.ceil(count / pageSize);
+    const { data, meta } = await getAllHistories(page, pageSize);
     res.json({
-      data: rows,
+      data,
       success: true,
       statusCode: '200',
-      meta: {
-        page: page + 1,
-        pageSize,
-        pages,
-      },
+      meta,
     });
   } catch (error) {
     next(error);
@@ -111,59 +54,12 @@ exports.delete = async (req, res, next) => {
 };
 
 exports.update = async (req, res, next) => {
-  const { history = {} } = req;
+  const { history = {}, userId } = req;
   const { ...historyData } = req.body;
-  const desiredHistoryData = omit(historyData, ['code_id', 'remove_codes']);
   try {
-    const result = await db.sequelize.transaction(async (t) => {
-      const historyUpdated = await history.update({
-        ...desiredHistoryData,
-        updated_by_id: req.userId,
-      });
-      if (req.body.patient_id) {
-        const patientFound = await Patient.findOne({
-          where: {
-            id: req.body.patient_id,
-          },
-        }, { transaction: t });
-        await historyUpdated.setPatient(patientFound);
-      }
-      if (req.body.agreement_id) {
-        const agreementFound = await Agreement.findOne({
-          where: {
-            id: req.body.agreement_id,
-          },
-        }, { transaction: t });
-        await historyUpdated.setAgreement(agreementFound);
-      }
-      if (historyData.code_id) {
-        const codesFound = await Code.findAll({
-          where: {
-            id: {
-              [Op.or]: historyData.code_id,
-            },
-          },
-        }, { transaction: t });
-        await historyUpdated.setCodes(codesFound);
-      }
-      if (historyData.remove_codes) {
-        const removeCodesFound = await Code.findAll({
-          where: {
-            id: {
-              [Op.or]: historyData.remove_codes,
-            },
-          },
-        }, { transaction: t });
-        await historyUpdated.removeCodes(removeCodesFound);
-      }
-
-      return historyUpdated;
-    });
-
-    const reloadResult = await result.reload();
-
+    const historyUpdated = await updateHistory(history, historyData, userId);
     res.json({
-      data: reloadResult,
+      data: historyUpdated,
       success: true,
       statusCode: '200',
     });
@@ -174,51 +70,11 @@ exports.update = async (req, res, next) => {
 
 exports.create = async (req, res, next) => {
   // Save History to Database
+  const { userId } = req;
   const { ...historyData } = req.body;
   try {
-    if (req.body.patient_id && req.body.agreement_id && req.body.code_id) {
-      const history = await History.create({
-        id: uuidv4(),
-        ...historyData,
-        created_by_id: req.userId,
-        updated_by_id: req.userId,
-      });
-      await Patient.findOne({
-        where: {
-          id: req.body.patient_id,
-        },
-      }).then((patient) => {
-        history.setPatient(patient);
-      });
-      await Agreement.findOne({
-        where: {
-          id: req.body.agreement_id,
-        },
-      }).then((agreement) => {
-        history.setAgreement(agreement);
-      });
-      await Code.findAll({
-        where: {
-          id: {
-            [Op.or]: req.body.code_id,
-          },
-        },
-      }).then((codes) => {
-        history.setCodes(codes).then(() => {
-          res.send({ message: 'History was registered successfully!' });
-        });
-      });
-    } else {
-      const errorMessage = 'History required fields';
-      const errorArray = [];
-      if (!req.body.patient_id) errorArray.push('Patient');
-      if (!req.body.agreement_id) errorArray.push('Agreement');
-      if (!req.body.code_id) errorArray.push('CIE Code');
-      next({
-        statusCode: '400',
-        message: `${errorMessage}: ${errorArray.join(', ')}`,
-      });
-    }
+    await createHistory(historyData, userId);
+    res.send({ message: 'History was registered successfully!' });
   } catch (error) {
     next({
       statusCode: '404',
---
+++ src/services/historyService.js
@@ -0,0 +1,186 @@
+/* eslint-disable camelcase */
+const { v4: uuidv4 } = require('uuid');
+const validator = require('validator');
+const omit = require('lodash/omit');
+const paginate = require('../utils/paginate');
+const db = require('../models');
+
+const Patient = db.patient;
+const History = db.history;
+const City = db.city;
+const State = db.state;
+const Code = db.code;
+const Agreement = db.agreement;
+const { Op } = db.Sequelize;
+
+async function getHistory(id) {
+  try {
+    if (validator.isUUID(id)) {
+      const history = await History.findOne({
+        where: {
+          id,
+        },
+        include: [
+          {
+            model: Code,
+          },
+          {
+            model: Patient,
+            include: {
+              model: City,
+              include: [State],
+            },
+          },
+        ],
+      });
+      if (history) {
+        return history;
+      }
+      throw new Error({
+        statusCode: '404',
+        message: 'Resource not found',
+      });
+    } else {
+      throw new Error({
+        statusCode: '404',
+        message: 'Resource not found',
+      });
+    }
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
+async function getAllHistories(page, pageSize) {
+  try {
+    const { rows, count } = await History.findAndCountAll({
+      where: {},
+      ...paginate({ page, pageSize }),
+      include: [
+        {
+          model: Patient,
+          include: [{
+            model: City,
+            include: [State],
+          }],
+        },
+      ],
+    });
+    const pages = Math.ceil(count / pageSize);
+    return {
+      data: rows,
+      meta: {
+        page: page + 1,
+        pageSize,
+        pages,
+      },
+    };
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
+async function updateHistory(history, historyData, userId) {
+  const desiredHistoryData = omit(historyData, ['code_id', 'remove_codes']);
+  try {
+    const result = await db.sequelize.transaction(async (t) => {
+      const historyUpdated = await history.update({
+        ...desiredHistoryData,
+        updated_by_id: userId,
+      });
+      if (historyData.patient_id) {
+        const patientFound = await Patient.findOne({
+          where: {
+            id: historyData.patient_id,
+          },
+        }, { transaction: t });
+        await historyUpdated.setPatient(patientFound);
+      }
+      if (historyData.agreement_id) {
+        const agreementFound = await Agreement.findOne({
+          where: {
+            id: historyData.agreement_id,
+          },
+        }, { transaction: t });
+        await historyUpdated.setAgreement(agreementFound);
+      }
+      if (historyData.code_id) {
+        const codesFound = await Code.findAll({
+          where: {
+            id: {
+              [Op.or]: historyData.code_id,
+            },
+          },
+        }, { transaction: t });
+        await historyUpdated.setCodes(codesFound);
+      }
+      if (historyData.remove_codes) {
+        const removeCodesFound = await Code.findAll({
+          where: {
+            id: {
+              [Op.or]: historyData.remove_codes,
+            },
+          },
+        }, { transaction: t });
+        await historyUpdated.removeCodes(removeCodesFound);
+      }
+
+      return historyUpdated;
+    });
+
+    const reloadResult = await result.reload();
+
+    return reloadResult;
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
+// eslint-disable-next-line consistent-return
+async function createHistory(historyData, userId) {
+  try {
+    if (historyData.patient_id && historyData.agreement_id && historyData.code_id) {
+      const history = await History.create({
+        id: uuidv4(),
+        ...historyData,
+        created_by_id: userId,
+        updated_by_id: userId,
+      });
+      await Patient.findOne({
+        where: {
+          id: historyData.patient_id,
+        },
+      }).then((patient) => {
+        history.setPatient(patient);
+      });
+      await Agreement.findOne({
+        where: {
+          id: historyData.agreement_id,
+        },
+      }).then((agreement) => {
+        history.setAgreement(agreement);
+      });
+      await Code.findAll({
+        where: {
+          id: {
+            [Op.or]: historyData.code_id,
+          },
+        },
+      }).then((codes) => {
+        history.setCodes(codes);
+      });
+    }
+  } catch (error) {
+    throw new Error({
+      statusCode: '404',
+      message: error.message,
+    });
+  }
+}
+
+module.exports = {
+  getHistory,
+  getAllHistories,
+  updateHistory,
+  createHistory,
+};
